20148e8d9c0a6306b91a43530e421a4b6f87ef59
jingx <50790703+chooron@users.noreply.github.com>
2024-08-09 20:47:03 +0800

    调整模块加载

--- src/HydroErr.jl
+++ src/HydroErr.jl
@@ -3,9 +3,12 @@ using Statistics
 using DocStringExtensions
 
 
-files = filter(x -> endswith(x, ".jl"), readdir(pwd() * "\\src\\functions", join=true))
-
-for file in files
-    include(file)
-end
+include("functions/kge.jl")
+include("functions/me.jl")
+include("functions/mhe.jl")
+include("functions/mle.jl")
+include("functions/mse.jl")
+include("functions/nse.jl")
+include("functions/r2.jl")
+include("functions/rmse.jl")
 end
--- src/functions/mse.jl
+++ src/functions/mse.jl
@@ -1,3 +1,3 @@
 function mse(simulated_array::AbstractVector{T}, observed_array::AbstractVector{T}; kwargs...)::T where {T}
-    sum((observed_array .- simulated_array) .^ (2)) / length(target)
+    sum((observed_array .- simulated_array) .^ (2)) / length(observed_array)
 end
